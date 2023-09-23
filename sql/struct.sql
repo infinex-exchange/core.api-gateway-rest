cd07925992620b8cb2a7f00236e0b9702298cdb4
infinex-exchange <admin@infinex.cc>
2023-09-23 15:24:25 +0200

    Add sql

---
+++ sql/struct.sql
@@ -0,0 +1,8 @@
+CREATE ROLE core.api-gateway-rest LOGIN PASSWORD 'password';
+
+create table routes(
+    path varchar(255) not null,
+    service varchar(255) not null
+);
+
+GRANT SELECT ON routes TO core.api-gateway-rest;
