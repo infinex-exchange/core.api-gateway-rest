f557ba26a5df970d86e2e7763036d3cab90a6cfb
infinex-exchange <admin@infinex.cc>
2023-09-25 20:11:33 +0200

    Fix service name in sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,11 +1,11 @@
 insert into routes(path, service) values
-    ('/account', 'temp.api-legacy'),
-    ('/wallet', 'temp.api-legacy'),
-    ('/spot', 'temp.api-legacy'),
-    ('/info', 'temp.api-legacy'),
-    ('/bridge', 'temp.api-legacy'),
-    ('/mining', 'temp.api-legacy'),
-    ('/p2p', 'temp.api-legacy'),
-    ('/nft', 'temp.api-legacy'),
-    ('/gamble', 'temp.api-legacy'),
-    ('/ipc', 'temp.api-legacy');
+    ('/account', 'temp.legacy-api'),
+    ('/wallet', 'temp.legacy-api'),
+    ('/spot', 'temp.legacy-api'),
+    ('/info', 'temp.legacy-api'),
+    ('/bridge', 'temp.legacy-api'),
+    ('/mining', 'temp.legacy-api'),
+    ('/p2p', 'temp.legacy-api'),
+    ('/nft', 'temp.legacy-api'),
+    ('/gamble', 'temp.legacy-api'),
+    ('/ipc', 'temp.legacy-api');
