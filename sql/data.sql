1b07ba8cc2cbec7acab26aae0c4657bcd9ea6fa9
infinex-exchange <admin@infinex.cc>
2023-10-20 15:24:45 +0200

    popup

--- sql/data.sql
+++ sql/data.sql
@@ -5,4 +5,5 @@ insert into routes(path, service) values
     ('/affiliate/v2', 'affiliate.affiliate'),
     ('/wallet/v2', 'wallet.wallet'),
     ('/wallet/v2/io', 'wallet.io'),
-    ('/wallet/v2/addressbook', 'wallet.addressbook');
+    ('/wallet/v2/addressbook', 'wallet.addressbook'),
+    ('/info/v2/popup', 'info.popup');
